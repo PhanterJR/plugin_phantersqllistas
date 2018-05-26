264ccd8c71c15748a89edadaed0d69b7ec74d5f0
PhanterJR <junior.conex@gmail.com>
2018-05-26 00:55:10 -0300

    Documentando

--- controllers/plugin_phantersqllistas.py
+++ controllers/plugin_phantersqllistas.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def index():
+    #Exemplo de uso, observer que é necessário jquery, bootstrap 4 e suas dependências.
     conteudo_body=CAT()
     html=HTML(HEAD(LINK(_rel="stylesheet", _href=URL("static", "css", args=["bootstrap.min.css"])),
         LINK(_rel="stylesheet", _href=URL("static", "plugin_phantersqllistas", args=["css","plugin_phantersqllistas.css"])),
@@ -13,6 +14,7 @@ def index():
     return html
 
 def echo_phantersqllistas():
+    #Exemplo da função que vai receber as requisições de ordenar, paginar, pesquisar na tabela e retornar um json.
     import json
     ordem=request.vars.ordem
     sentido=request.vars.sentido
@@ -23,12 +25,12 @@ def echo_phantersqllistas():
     pagina=int(request.vars.pagina)
     if palavra:
         frase="Pesquisando em \"%s\" por \"%s\"" %(db[tabela][campo].label, palavra)
-        MODELS_PHANTERSQLLISTAS_VEICULOS.setAviso(frase)
-        api=MODELS_PHANTERSQLLISTAS_VEICULOS.echoAPI(ordem, sentido , campo , palavra , num_registros , pagina)
+        MODELS_PHANTERSQLLISTAS.setAviso(frase)
+        api=MODELS_PHANTERSQLLISTAS.echoAPI(ordem, sentido , campo , palavra , num_registros , pagina)
         return response.json(api)
     else:
         frase=""
-        MODELS_PHANTERSQLLISTAS_VEICULOS.setAviso(frase)
-        api=MODELS_PHANTERSQLLISTAS_VEICULOS.echoAPI(ordem, sentido , campo , palavra , num_registros , pagina)
+        MODELS_PHANTERSQLLISTAS.setAviso(frase)
+        api=MODELS_PHANTERSQLLISTAS.echoAPI(ordem, sentido , campo , palavra , num_registros , pagina)
         return response.json(api)
 
